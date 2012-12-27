2768a8b743f0bffe50b8d262df0f1cb0502e5132
Ezra Pool <ezra@tsdme.nl>
2012-12-27 17:57:39 +0100

    License update

--- Lib/Cache/Engine/MemcachedEngine.php
+++ Lib/Cache/Engine/MemcachedEngine.php
@@ -3,11 +3,27 @@
 /**
  * Memcached storage engine for CakePHP
  *
- * PHP versions 4 and 5
- *
  * @author Ezra Pool <ezra@tsdme.nl>
- * @copyright BSD
  * @version 0.2.1
+ * @version PHP 5
+ * @version CakePHP 2.*
+ */
+/**
+ * @copyright (c) 2012, Ezra Pool <ezra@tsdme.nl>
+ * @license LGPL v3
+ *
+ * This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -245,7 +261,6 @@ class MemcachedEngine extends CacheEngine {
 	function clear($delay) {
 		return $this->__Memcached->flush($delay);
 	}
-
 }
 
 ?>
